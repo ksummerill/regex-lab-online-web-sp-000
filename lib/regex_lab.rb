1c689feee77684dc1e793ab8d06ce816f89d7ebb
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-12 14:39:49 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,28 @@
 def starts_with_a_vowel?(word)
-
+  if word.match(/\A[aeiouAEIOU]/)
+    return true
+  else false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  if text.scan(/\A[un]+\z[ing]/)
+    return text.scan(text)
+  end
 end
+
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
-def first_word_capitalized_and_ends_with_punctuation?(text)
-
-end
-
+# def first_word_capitalized_and_ends_with_punctuation?(text)
+#
+# end
+#
 def valid_phone_number?(phone)
-
+  if phone.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
+    return true
+  else false
+  end
 end
