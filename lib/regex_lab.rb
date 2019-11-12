56878433e8a64ccf56aaff0eac3e8e177bed6fe2
Kate Summerill <kaitlynsummerill@gmail.com>
2019-11-12 14:49:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -14,10 +14,14 @@ def words_five_letters_long(text)
   text.scan(/\b\w{5}\b/)
 end
 
-# def first_word_capitalized_and_ends_with_punctuation?(text)
-#
-# end
-#
+# Returns true for text starting with a capital letter and ending with puncutation
+def first_word_capitalized_and_ends_with_punctuation?(text)
+  if text.match(//)
+    return true
+  else false
+  end    
+end
+
 def valid_phone_number?(phone)
   if phone.scan(/\A(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}\z/)
     return true
